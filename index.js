cdc2f875bdf01890af7304e745411a9877d9ec2d
Ethan <180671720+3thanlarson@users.noreply.github.com>
2026-03-26 22:43:01 -0500

    Added hover functionality and altered colors temporarily

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ const gridContainer = document.getElementById("grid-container");
 
 let gridWidth = 16;
 
-for (let j = 0; j < gridWidth; j++) {
-    const newGridLine = document.createElement("div");
-    newGridLine.style.display = "flex";
-    newGridLine.style.flexDirection = "row";
-    gridContainer.appendChild(newGridLine);
-    for (let i = 0; i < gridWidth; i++) {
-        const newGridSquare = document.createElement("div");
-        newGridLine.appendChild(newGridSquare);
-        newGridSquare.style.display = "flex";
-        newGridSquare.style.padding = 250/gridWidth + "px";
-        newGridSquare.style.border = "solid, black"
+function createGrid(newGridWidth) {
+    for (let j = 0; j < newGridWidth; j++) {
+        const newGridLine = document.createElement("div");
+        newGridLine.style.display = "flex";
+        newGridLine.style.flexDirection = "row";
+        gridContainer.appendChild(newGridLine);
+        for (let i = 0; i < newGridWidth; i++) {
+            const newGridSquare = document.createElement("div");
+            newGridSquare.setAttribute('id', "grid-square");
+            newGridLine.appendChild(newGridSquare);
+            newGridSquare.style.display = "flex";
+            newGridSquare.style.padding = 250/newGridWidth + "px";
+            newGridSquare.style.border = "solid, black"
+        }
     }
 }
+
+createGrid(gridWidth);
+
+const gridSquares = document.querySelectorAll("#grid-square");
+//Add event listener for hover effect
+gridSquares.forEach(gridSquares => {
+    gridSquares.addEventListener('mouseover', function () {
+        this.style.backgroundColor = "black";
+    });
+});
